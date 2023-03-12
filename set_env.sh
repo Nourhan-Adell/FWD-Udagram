22c9748ae40387c42f8535c3a778ea4b5867e6d1
Nourhan Adel <nourhan1168@gmail.com>
2023-03-12 15:32:54 +0200

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=NourhanAdel
+export POSTGRES_HOST=database-2.cq5qaer41iw5.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-nourhan
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:4200
